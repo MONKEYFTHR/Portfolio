ced2750622b5180295ec73f2db932a1cd194f294
MONKEYFTHR <48908627+MONKEYFTHR@users.noreply.github.com>
2019-06-23 20:20:45 +0200

    Update bddportfolio.sql

--- portfolio1/bddportfolio.sql
+++ portfolio1/bddportfolio.sql
@@ -1,13 +1,18 @@
-CREATE DATABASE messagerie_1;
-USE messagerie;
+CREATE DATABASE MVC;
+USE MVC;
 CREATE TABLE admin (
-	id varchar(50) UNIQUE,
-  pass varchar(50) UNIQUE,
+  pass varchar(50) UNIQUE
 );
 
 CREATE TABLE message(
 	id int(11) PRIMARY KEY  NOT NULL auto_increment,
 	expediteur varchar(50),
 	date datetime NOT NULL,
-	message text NOT NULL,
+	message text NOT NULL
 );
+
+CREATE TABLE article(
+	nom varchar(50),
+	content varchar(1000)
+);
+INSERT INTO MVC(pass) VALUES('Jean'); 
